8652a229cf21b512babb0b71fec19e3297dc2814
STNox <caush@naver.com>
2020-12-22 18:33:22 +0900

    Covid Flask

--- Python-flask/03. Data-analysis Module/DB/db_module.py
+++ Python-flask/03. Data-analysis Module/DB/db_module.py
@@ -1,39 +1,30 @@
 import sqlite3
 import pandas as pd
 
-def create_table():
+def create_table(sql):
     conn = sqlite3.connect('./DB/covid_1.db')
     cur = conn.cursor()
-    sql_table = '''create table covid_status (
-        date text not null,
-        region text not null,
-        def_cnts int default 0,
-        local_occ int default 0,
-        over_flow int default 0,
-        inc_decs int default 0,
-        death_cnts int default 0,
-        isol_clr int default 0,
-        isol_ing int default 0, 
-        qur_rates float default 0
-    );'''
+    sql_table = sql
     cur.execute(sql_table)
     conn.commit()
     conn.close()
 
-def insert(df, db_name):
-    sql_insert = f'insert into {db_name} values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+def insert(df, sql, params):
+    sql_insert = sql
+    params = params
     for i in df.index:
-        params = [df['일시'][i], df['지역'][i], int(df['누적확진'][i]), int(df['국내발생'][i]), int(df['해외유입'][i]), int(df['전일대비 증감'][i]), int(df['사망'][i]), int(df['격리해제'][i]), int(df['격리중'][i])]
-        params.append(None if df['10만명당 발생률'][i] == '-' else float(df['10만명당 발생률'][i]))
-        conn = sqlite3.connect('covid_1.db')
+        conn = sqlite3.connect('./DB/covid_1.db')
         cur = conn.cursor()
         cur.execute(sql_insert, params)
         conn.commit()
         conn.close()
 
-def select(db_name):
-    conn = sqlite3.connect('covid_1.db')
+def get_status(date):
+    conn = sqlite3.connect('./DB/covid_1.db')
     cur = conn.cursor()
-    sql_select = f'select * from {db_name}'
-    cur.execute(sql_select)
+    sql_select = f'select * from covid_status where date=?;'
+    cur.execute(sql_select, (date,)) # 단순 변수를 주면 튜플이나 리스트 형태로, 리스트를 변수로 주면 변수명 그대로 쓴다.
+    rows = cur.fetchall()
+    cur.close()
     conn.close()
+    return rows
--- Python-flask/03. Data-analysis Module/bp2_covid/covid.py
+++ Python-flask/03. Data-analysis Module/bp2_covid/covid.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, current_app
 import pandas as pd
 import folium, os, json
+from datetime import datetime
 from utils.weather import cur_weather
 import DB.covid_db as cov
 import DB.db_module as dm
@@ -8,13 +9,13 @@ import DB.db_module as dm
 covid_bp = Blueprint('covid_bp', __name__)
 
 menu = {'ho': 0, 'da': 1, 'ml': 0, 'se': 0, 'co': 1, 'ca': 0, 'cr': 0, 'st': 0, 'wc': 0}
-covid_status = dm.select(covid_status)
-@covid_bp.route('/n_status/<option>')
-def national_status(option):
-    map = folium.Map(location=[36.2002, 127.054], zoom_start=7)
-    folium.Rectangle(
-        
-    )
+
+@covid_bp.route('/national_status')
+def national_status():
+    date = request.args.get('date', datetime.now().strftime('%Y-%m-%d'))
+    rows = dm.get_status(date)
+
+    return render_template('covid/status.html', menu=menu, weather=cur_weather(), rows=rows, date=date)
 
 # DB 채워 넣고 화면에 띄우기
 # covid_1.db 쓸 것
